dc2fb85a7011a48453059badf9caa60ddb80866f
Michael Kondrashin <mkondrashin@gmail.com>
2023-08-08 15:14:43 +0300

    add comments

--- search_get_endpoint_data.go
+++ search_get_endpoint_data.go
@@ -4,7 +4,7 @@
 
 	Sandbox API capabilities
 
-	sandbox_daily_reserve.go - get dalily usage quota
+	search_get_endpoint_data.go - get endpoint data
 */
 
 package vone
@@ -15,6 +15,7 @@ import (
 	"time"
 )
 
+// SearchEndPointDataResponseItem - get endpoint data response for each endpoint
 type SearchEndPointDataResponseItem struct {
 	AgentGUID    string `json:"agentGuid"`
 	LoginAccount struct {
@@ -40,17 +41,20 @@ type SearchEndPointDataResponseItem struct {
 	InstalledProductCodes []string `json:"installedProductCodes"`
 }
 
+// SearchEndPointDataResponse - get endpoint data response
 type SearchEndPointDataResponse struct {
 	Items    []SearchEndPointDataResponseItem `json:"items"`
 	NextLink string                           `json:"nextLink"`
 }
 
+// SearchEndPointDataFunc - search for endpoints
 type SearchEndPointDataFunc struct {
 	BaseFunc
 	Response SearchEndPointDataResponse
 	query    string
 }
 
+// Do - run request
 func (f *SearchEndPointDataFunc) Do(ctx context.Context) (*SearchEndPointDataResponse, error) {
 	if err := f.vone.Call(ctx, f); err != nil {
 		return nil, err
@@ -66,6 +70,8 @@ func (f *SearchEndPointDataFunc) Next(ctx context.Context) (*SearchEndPointDataR
 	return &f.Response, nil
 }
 
+// Iterate - get all endpoints matching query one by one. If callback returns
+// non nil error, iteration is aborted and this error is returned
 func (f *SearchEndPointDataFunc) Iterate(ctx context.Context,
 	callback func(item *SearchEndPointDataResponseItem) error) error {
 	for {
@@ -85,6 +91,7 @@ func (f *SearchEndPointDataFunc) Iterate(ctx context.Context,
 	return nil
 }
 
+// SearchEndPointData - get new search for endpoint data function
 func (v *VOne) SearchEndPointData() *SearchEndPointDataFunc {
 	f := &SearchEndPointDataFunc{}
 	f.BaseFunc.Init(v)
@@ -92,6 +99,7 @@ func (v *VOne) SearchEndPointData() *SearchEndPointDataFunc {
 
 }
 
+// Query - set search query
 func (f *SearchEndPointDataFunc) Query(query string) *SearchEndPointDataFunc {
 	if f.query != query {
 		f.query = query
@@ -100,6 +108,7 @@ func (f *SearchEndPointDataFunc) Query(query string) *SearchEndPointDataFunc {
 	return f
 }
 
+// top - set limit for returned amount of items
 func (f *SearchEndPointDataFunc) Top(t Top) *SearchEndPointDataFunc {
 	f.SetParameter("top", t.String())
 	return f
