15610b91cd32bf5edce90e75b08909d559c61d72
Michael Kondrashin <mkondrashin@gmail.com>
2026-01-02 15:16:50 +0200

    add mockup 2

---
+++ sandbox_mockup.go
@@ -0,0 +1,189 @@
+package vone
+
+import (
+	"crypto/md5"
+	"crypto/sha1"
+	"crypto/sha256"
+	"encoding/json"
+	"fmt"
+	"io"
+	"log"
+	"os"
+	"strings"
+	"time"
+
+	"github.com/google/uuid"
+)
+
+type SubmissionState int
+
+const (
+	state SubmissionState = iota
+)
+
+type (
+	submission struct {
+		state            SubmissionState
+		analysisResult   SandboxAnalysisResultsResponse
+		submissionStatus SandboxSubmissionStatusResponse
+	}
+
+	SandboxMockup struct {
+		submissions map[string]*submission
+		logger      *log.Logger
+	}
+)
+
+func NewSandboxMockup() *SandboxMockup {
+	return &SandboxMockup{
+		submissions: make(map[string]*submission),
+		logger:      log.New(io.Discard, "", 0),
+	}
+}
+
+func (s *SandboxMockup) EnableFileLogging(path string) error {
+	f, err := os.OpenFile(path, os.O_CREATE|os.O_APPEND|os.O_WRONLY, 0644)
+	if err != nil {
+		return err
+	}
+	s.logger = log.New(
+		f,
+		"",
+		log.LstdFlags,
+	)
+	return nil
+}
+
+func (sm *SandboxMockup) SubmitFile(f *SandboxSubmitFileToSandboxFunc) (*SandboxSubmitFileResponse, *SandboxSubmitFileResponseHeaders, error) {
+	sm.logger.Println("SubmitFile")
+	data, err := io.ReadAll(f.Request)
+	if err != nil {
+		return nil, nil, err
+	}
+	md5Hash := md5.Sum(data)
+	sha1Hash := sha1.Sum(data)
+	sha256Hash := sha256.Sum256(data)
+	digest := Digest{
+		MD5:    fmt.Sprintf("%x", md5Hash),
+		SHA1:   fmt.Sprintf("%x", sha1Hash),
+		SHA256: fmt.Sprintf("%x", sha256Hash),
+	}
+	id := uuid.New().String()
+	sm.logger.Printf("SubmitFile (%s)", id)
+	response := SandboxSubmitFileResponse{
+		ID:        id,
+		Digest:    digest,
+		Arguments: "",
+	}
+
+	headers := SandboxSubmitFileResponseHeaders{
+		OperationLocation:        "",
+		SubmissionReserveCount:   100,
+		SubmissionRemainingCount: 100,
+		SubmissionCount:          1,
+		SubmissionExemptionCount: 1,
+	}
+
+	sub := &submission{}
+	sm.submissions[id] = sub
+
+	err = json.Unmarshal(data, &sub.submissionStatus)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unmarshal: %w", err)
+	}
+	if sub.submissionStatus.Status == StatusFailed {
+		sm.logger.Printf("SubmitFile (%s): failed", id)
+		sub.submissionStatus.ID = id
+		sub.submissionStatus.Action = ActionAnalyzeFile
+		sub.submissionStatus.CreatedDateTime = VisionOneTime(time.Now())
+		sub.submissionStatus.LastActionDateTime = VisionOneTime(time.Now())
+		sub.submissionStatus.IsCached = false
+		sub.submissionStatus.Digest = digest
+		return &response, &headers, nil
+	}
+
+	err = json.Unmarshal(data, &sub.analysisResult)
+	if err != nil {
+		return nil, nil, fmt.Errorf("unmarshal: %w", err)
+	}
+	sm.logger.Printf("SubmitFile (%s): success", id)
+	sub.analysisResult.ID = id
+	sub.analysisResult.Type = "file"
+	sub.analysisResult.Digest = digest
+
+	sub.submissionStatus = SandboxSubmissionStatusResponse{
+		ID:                 id,
+		Action:             ActionAnalyzeFile,
+		Status:             StatusRunning,
+		Error:              Error{},
+		CreatedDateTime:    VisionOneTime(time.Now()),
+		LastActionDateTime: VisionOneTime(time.Now()),
+		ResourceLocation:   "",
+		IsCached:           false,
+		Digest:             digest,
+		Arguments:          "",
+	}
+	sm.logger.Printf("SubmitFile (%s): created submission with status %v", id, sub.submissionStatus.Status)
+	sm.logger.Printf("SubmitFile (%s): created analysis result with risk level %v", id, sub.analysisResult.RiskLevel)
+	return &response, &headers, nil
+}
+
+func (sm *SandboxMockup) SubmissionStatus(f *SandboxSubmissionStatusFunc) (*SandboxSubmissionStatusResponse, error) {
+	sm.logger.Printf("SubmissionStatus (%s)", f.id)
+	submission, ok := sm.submissions[f.id]
+	if !ok {
+		return nil, fmt.Errorf("submission %s not found", f.id)
+	}
+	result := submission.submissionStatus
+	submission.submissionStatus.Status = StatusSucceeded
+	return &result, nil
+}
+
+func (sm *SandboxMockup) AnalysisResults(f *SandboxAnalysisResultsFunc) (*SandboxAnalysisResultsResponse, error) {
+	sm.logger.Printf("AnalysisResults (%s)", f.id)
+	submission, ok := sm.submissions[f.id]
+	sm.logger.Printf("AnalysisResults (%s): Submission found: %v", f.id, ok)
+	if !ok {
+		return nil, fmt.Errorf("submission %s not found", f.id)
+	}
+	if submission.submissionStatus.Status != StatusSucceeded {
+		return nil, fmt.Errorf("submission %s status is not succeed", f.id)
+	}
+	sm.logger.Printf("AnalysisResults (%s): returning risk level %v", f.id, submission.analysisResult.RiskLevel)
+	return &submission.analysisResult, nil
+}
+
+func (sm *SandboxMockup) ListSubmissions(f *SandboxSubmissionsFunc) (*SandboxSubmissionsResponse, error) {
+	sm.logger.Printf("ListSubmissions")
+	response := SandboxSubmissionsResponse{
+		Items:    nil,
+		NextLink: "",
+	}
+	for id, s := range sm.submissions {
+		if strings.Contains(f.parameters["filter"], id) {
+			response.Items = append(response.Items, s.submissionStatus)
+			s.submissionStatus.Status += 1
+		}
+	}
+	sm.logger.Printf("ListSubmissions: %d items", len(response.Items))
+	return &response, nil
+}
+
+func (sm *SandboxMockup) ListAnalysisResults(f *SandboxListAnalysisResultsFunc) (*SandboxListAnalysisResultResponse, error) {
+	sm.logger.Printf("ListAnalysisResults")
+	results := SandboxListAnalysisResultResponse{
+		Items:    nil,
+		NextLink: "",
+	}
+	for id, s := range sm.submissions {
+		if !strings.Contains(f.parameters["filter"], id) {
+			continue
+		}
+		if s.submissionStatus.Status != StatusSucceeded {
+			continue
+		}
+		results.Items = append(results.Items, s.analysisResult)
+	}
+	sm.logger.Printf("ListAnalysisResults: %d items", len(results.Items))
+	return &results, nil
+}
---
+++ sandbox_mockup_test.go
@@ -0,0 +1,98 @@
+package vone
+
+import (
+	"bytes"
+	"context"
+	"os"
+	"testing"
+)
+
+func TestSandboxMockup(t *testing.T) {
+	v1 := NewVOne("https://api.trendmicro.com", "token")
+	mockup := NewSandboxMockup()
+	mockupFilePath := "mockup.log"
+	if err := os.RemoveAll(mockupFilePath); err != nil {
+		t.Fatalf("RemoveAll Error: %v", err)
+	}
+	if err := mockup.EnableFileLogging(mockupFilePath); err != nil {
+		t.Fatalf("EnableFileLogging Error: %v", err)
+	}
+	v1.SetMockup(mockup)
+	t.Logf("Test Submit File with failed status")
+	submitFile := v1.SandboxSubmitFile()
+	submitFile.Request = bytes.NewReader([]byte(`{
+  "status": "failed",
+  "error": {
+    "code": "InternalServerError",
+    "message": "error_message"
+  }
+}`))
+	response, headers, err := submitFile.Do(context.Background())
+	if err != nil {
+		t.Errorf("Submit File Error: %v", err)
+	}
+	id := response.ID
+	t.Logf("response: ID=%v", id)
+	t.Logf("headers: SubmissionCount=%v, SubmissionRemainingCount=%v", headers.SubmissionCount, headers.SubmissionRemainingCount)
+	t.Logf("Get Submission Status")
+	submisionStatus := v1.SandboxSubmissionStatus(id)
+	status, err := submisionStatus.Do(context.Background())
+	if err != nil {
+		t.Errorf("Submission Status Error: %v", err)
+	}
+	t.Logf("status: %v", status.Status)
+	t.Logf("Test Submit Malicious File")
+	submitFile = v1.SandboxSubmitFile()
+	submitFile.Request = bytes.NewReader([]byte(`{
+  "riskLevel": "high",
+  "detectionNames": [
+    "VAN_DROPPER.UMXX"
+  ],
+  "threatTypes": [
+    "Dropper"
+  ],
+  "trueFileType": "exe"
+}`))
+	response, headers, err = submitFile.Do(context.Background())
+	if err != nil {
+		t.Errorf("Submit Malicious File Error: %v", err)
+	}
+	id = response.ID
+	t.Logf("response: ID assigned %v", id)
+	t.Logf("headers: SubmissionCount=%v, SubmissionRemainingCount=%v", headers.SubmissionCount, headers.SubmissionRemainingCount)
+	t.Logf("Submission: %v", v1.mockup.submissions[id].submissionStatus.Status)
+	t.Logf("Get Analysis Result For Not ready result")
+	analysisResult := v1.SandboxAnalysisResults(id)
+	result, err := analysisResult.Do(context.Background())
+	if err != nil {
+		t.Errorf("Analysis Results Error: %v", err)
+	}
+	if result != nil {
+		t.Fatalf("No ready result is returned!: %v", result)
+	}
+	t.Logf("Get First Submission Status for Malicious File")
+	submisionStatus = v1.SandboxSubmissionStatus(id)
+	status, err = submisionStatus.Do(context.Background())
+	if err != nil {
+		t.Errorf("Submission Malicious FileStatus Error: %v", err)
+	}
+	t.Logf("status: %v", status.Status)
+	t.Logf("Get Second Submission Status for Malicious File")
+	submisionStatus = v1.SandboxSubmissionStatus(id)
+	status, err = submisionStatus.Do(context.Background())
+	if err != nil {
+		t.Errorf("Submission Malicious FileStatus Error: %v", err)
+	}
+	t.Logf("status: %v", status.Status)
+	if err != nil {
+		t.Errorf("Submission Malicious FileStatus Error: %v", err)
+	}
+	t.Logf("Get Analysis Result For Malicious File")
+	analysisResult = v1.SandboxAnalysisResults(id)
+	result, err = analysisResult.Do(context.Background())
+	if err != nil {
+		t.Errorf("Analysis Results Error: %v", err)
+	}
+	t.Logf("result: RiskLevel=%v, DetectionNames=%v, ThreatTypes=%v", result.RiskLevel, result.DetectionNames, result.ThreatTypes)
+	t.Logf("Test Complete")
+}
